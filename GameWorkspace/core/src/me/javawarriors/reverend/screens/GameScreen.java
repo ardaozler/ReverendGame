276332c9cb512d3fb02ebb9b697ef8bfd8a0b1b5
Dincer İnce <dincer-ince00@gmail.com>
2020-12-22 00:32:13 +0300

    map added

--- GameWorkspace/core/src/me/javawarriors/reverend/screens/GameScreen.java
+++ GameWorkspace/core/src/me/javawarriors/reverend/screens/GameScreen.java
@@ -4,9 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 import me.javawarriors.reverend.ReverendGame;
 
@@ -30,6 +34,11 @@ public class GameScreen implements Screen {
 	public static final float charAnimationSpeed = 0.1f;
 	
 	
+	//map
+	private TiledMap map;
+	private OrthogonalTiledMapRenderer renderer;
+	private OrthographicCamera camera;
+	
 	ReverendGame game;
 
 	public GameScreen(ReverendGame game) {
@@ -44,7 +53,12 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void show() {
-
+		TmxMapLoader loader = new TmxMapLoader();
+		map= loader.load("try2.tmx");
+		renderer= new OrthogonalTiledMapRenderer(map, 4f);
+		
+		camera= new OrthographicCamera();
+		
 	}
 
 	public void move() {
@@ -82,11 +96,13 @@ public class GameScreen implements Screen {
 	@Override
 	public void render(float delta) {
 
-		Gdx.gl.glClearColor(0.5f, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+		renderer.setView(camera);
+		renderer.render();
+		camera.setToOrtho(false);
 		stateTime += delta;
-
+camera.update();
 		move();
 
 		game.getBatch().begin();
@@ -97,7 +113,9 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void resize(int width, int height) {
-
+camera.viewportHeight=1080;
+camera.viewportWidth=1920;
+camera.update();
 	}
 
 	@Override
@@ -117,7 +135,8 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void dispose() {
-		
+		map.dispose();
+		renderer.dispose();
 	}
 
 }
